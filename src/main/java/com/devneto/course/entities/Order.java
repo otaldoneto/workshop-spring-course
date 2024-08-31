6c3b7f0ee7bde2ef9e0f19ac783eab4161bb24d4
Ismael Neto <otaldoneto22@outlook.com>
2024-08-31 10:37:30 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/devneto/course/entities/Order.java
+++ src/main/java/com/devneto/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.devneto.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.devneto.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,13 +15,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -32,6 +35,9 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 	}
@@ -75,6 +81,10 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
---
+++ src/main/java/com/devneto/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.devneto.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devneto.course.entities.OrderItem;
+import com.devneto.course.entities.pk.OrderItemPK;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
+
+}
