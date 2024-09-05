4706e8fa86c7589b8f9aad1aa199ee7d4ce97a65
Ismael Neto <otaldoneto22@outlook.com>
2024-09-05 09:11:49 -0300

    Payment, one-to-one association

--- src/main/java/com/devneto/course/config/TestConfig.java
+++ src/main/java/com/devneto/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devneto.course.entities.Category;
 import com.devneto.course.entities.Order;
 import com.devneto.course.entities.OrderItem;
+import com.devneto.course.entities.Payment;
 import com.devneto.course.entities.Product;
 import com.devneto.course.entities.User;
 import com.devneto.course.entities.enums.OrderStatus;
@@ -81,6 +82,8 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
-		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}	
 }
